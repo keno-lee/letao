bda4a44040b9556ddf474c0f12ee39d671a12acd
keno.lee <keno.lee@foxmail.com>
2018-04-14 15:53:43 +0800

    升级下拉刷新+上拉加载+价格&库存排序-2.0版本

--- public/front/js/cart.js
+++ public/front/js/cart.js
@@ -1,40 +1,9 @@
 $(function () {
 
-    //初始化区域滚动
-    mui('.mui-scroll-wrapper').scroll({
-        scrollY: true, //是否竖向滚动
-        scrollX: false, //是否横向滚动
-        startX: 0, //初始化时滚动至x
-        startY: 0, //初始化时滚动至y
-        indicators: true, //是否显示滚动条
-        deceleration: 0.0006, //阻尼系数,系数越小滑动越灵敏
-        bounce: true //是否启用回弹
-    });
-
-    render();
-
-    function render() {
-        $.ajax({
-            url: "/cart/queryCart",
-            success: function (info) {
-                console.log(info);
-                if (info.error === 400) {
-                    var last = location.href;
-                    location.href = "login.html?last=" + last;
-                    return;
-                }
-                $(".lt_main ul").html(template("cartTmp", {
-                    list: info
-                }));
-
-            }
-        });
-    }
-
     // 注册编辑和删除功能
     var id;
-    // 1.删除
-    $(".lt_main").on("click", ".btn-del", function () {
+    // 1.删除 结束后触发下拉刷新
+    $(".lt_main").on("tap", ".btn-del", function () {
         id = $(this).parents("li").data("id");
         // console.log(id);
         mui.confirm('您确认删除该商品吗?', '温馨提示', ['取消', '确认'], function (e) {
@@ -47,7 +16,8 @@ $(function () {
                     success: function (info) {
                         console.log(info);
                         if (info.success) {
-                            render();
+                            // render();
+                            mui('#refreshContainer').pullRefresh().pulldownLoading();
                         }
                     }
 
@@ -57,7 +27,7 @@ $(function () {
     });
 
     // 2.编辑
-    $(".lt_main").on("click", ".btn-edit", function () {
+    $(".lt_main").on("tap", ".btn-edit", function () {
         var data = $(this).parents("li")[0].dataset;
         console.log(data);
 
@@ -81,8 +51,8 @@ $(function () {
                     },
                     success: function (info) {
                         console.log(info);
-                        if(info.success){
-                            render();
+                        if (info.success) {
+                            mui('#refreshContainer').pullRefresh().pulldownLoading();
                         }
 
                     }
@@ -99,5 +69,51 @@ $(function () {
     });
 
 
+    //初始化区域滚动
+    // mui('.mui-scroll-wrapper').scroll({
+    //     scrollY: true, //是否竖向滚动
+    //     scrollX: false, //是否横向滚动
+    //     startX: 0, //初始化时滚动至x
+    //     startY: 0, //初始化时滚动至y
+    //     indicators: true, //是否显示滚动条
+    //     deceleration: 0.0006, //阻尼系数,系数越小滑动越灵敏
+    //     bounce: true //是否启用回弹
+    // });
+
+    mui.init({
+        pullRefresh : {
+          container:"#refreshContainer",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+          down : {
+            height:50,//可选,默认50.触发下拉刷新拖动距离,
+            auto: true,//可选,默认false.首次加载自动下拉刷新一次
+            contentdown : "下拉可以刷新",//可选，在下拉可刷新状态时，下拉刷新控件上显示的标题内容
+            contentover : "释放立即刷新",//可选，在释放可刷新状态时，下拉刷新控件上显示的标题内容
+            contentrefresh : "正在刷新...",//可选，正在刷新状态时，下拉刷新控件上显示的标题内容
+            callback: function () {
+                setTimeout(function () {
+                    render();
+                },500);
+            } //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+          }
+        }
+      });
+
+      function render() {
+        $.ajax({
+            url: "/cart/queryCart",
+            success: function (info) {
+                console.log(info);
+                if (info.error === 400) {
+                    var last = location.href;
+                    location.href = "login.html?last=" + last;
+                    return;
+                }
+                $(".lt_main ul").html(template("cartTmp", {
+                    list: info
+                }));
+              mui('#refreshContainer').pullRefresh().endPulldownToRefresh();
+            }
+        });
+    }
 
 });
--- public/front/js/searchList.js
+++ public/front/js/searchList.js
@@ -1,19 +1,7 @@
-$(function() {
-
-
+$(function () {
 
     var currentPage = 1;
-    var pageSize = 5;
-    //初始化区域滚动
-    mui('.mui-scroll-wrapper').scroll({
-        scrollY: true, //是否竖向滚动
-        scrollX: false, //是否横向滚动
-        startX: 0, //初始化时滚动至x
-        startY: 0, //初始化时滚动至y
-        indicators: true, //是否显示滚动条
-        deceleration: 0.0006, //阻尼系数,系数越小滑动越灵敏
-        bounce: true //是否启用回弹
-    });
+    var pageSize = 4;
 
     function getHistory() {
         var history = localStorage.getItem("history") || '[]';
@@ -27,11 +15,8 @@ $(function() {
     $(".search-content").val(arr[0]);
 
 
-    // 进入页面开始渲染
-    render();
-
     // 点击搜索开始渲染
-    $(".btn-search").on("click", function() {
+    $(".btn-search").on("click", function () {
         //修改localstorage
         var arr = getHistory();
         var inputText = $(".search-content").val();
@@ -49,24 +34,88 @@ $(function() {
         arr.unshift(inputText);
         // 3.存储到本地存储中
         localStorage.setItem("history", JSON.stringify(arr));
-        render();
+        mui('#refreshContainer').pullRefresh().pulldownLoading();
     });
 
-    function render() {
+
+
+    //给所有的li注册点击事件//price 1升 2降序 sortPrice   num  1升 2将 sortNum
+    $(".sortItem").on("tap", function () {
+        if ($(this).hasClass("active")) {
+            $(this).find("i").toggleClass("fa-angle-down").toggleClass("fa-angle-up");
+        } else {
+            $(this).addClass("active").siblings().removeClass("active");
+            $(this).siblings().find("i").addClass("fa-angle-down").removeClass("fa-angle-up");
+        }
+        // 执行一次刷新
+        mui('#refreshContainer').pullRefresh().pulldownLoading();
+    });
+
+    mui.init({
+        pullRefresh: {
+            container: "#refreshContainer", //下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+            down: {
+                height: 50, //可选,默认50.触发下拉刷新拖动距离,
+                auto: true, //可选,默认false.首次加载自动下拉刷新一次
+                contentdown: "下拉可以刷新", //可选，在下拉可刷新状态时，下拉刷新控件上显示的标题内容
+                contentover: "释放立即刷新", //可选，在释放可刷新状态时，下拉刷新控件上显示的标题内容
+                contentrefresh: "正在刷新...", //可选，正在刷新状态时，下拉刷新控件上显示的标题内容
+                callback: function () {
+                    // 重置当前页
+                    currentPage = 1;
+                    setTimeout(function () {
+                        render(function (info) {
+                            $(".product ul").html(template("productTmp", info));
+                            mui('#refreshContainer').pullRefresh().endPulldownToRefresh();
+                                        // 重新启用上拉加载
+                            mui('#refreshContainer').pullRefresh().enablePullupToRefresh();
+                        });
+                    }, 500);
+                } //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+            },
+            up: {
+                height: 50, //可选.默认50.触发上拉加载拖动距离
+                auto: false, //可选,默认false.自动上拉加载一次
+                contentrefresh: "正在加载...", //可选，正在加载状态时，上拉加载控件上显示的标题内容
+                contentnomore: '没有更多数据了', //可选，请求完毕若没有更多数据时显示的提醒内容；
+                callback: function () {
+                    currentPage++;
+                    setTimeout(function () {
+                        render(function (info) {
+                            if (info.data.length == 0) {
+                                // 说明没有数据了
+                                mui('#refreshContainer').pullRefresh().endPullupToRefresh(true);
+                            } else {
+                                $(".product ul").append(template("productTmp", info));
+                                mui('#refreshContainer').pullRefresh().endPullupToRefresh();
+                            }
+                        });
+                    }, 500);
+                } //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+            }
+        }
+    });
+
+    function render(callback) {
         //获取表单内的值proName
         var proName = $(".search-content").val();
-        console.log(proName);
+        // 默认降序
+        var sortPrice = $(".sortPrice").find("i").hasClass("fa-angle-down") ? 2 : 1;
+        var sortNum = $(".sortNum").find("i").hasClass("fa-angle-down") ? 2 : 1;
+        // console.log(proName);
         $.ajax({
             url: "/product/queryProduct",
             type: "get",
             data: {
                 proName: proName,
                 page: currentPage,
-                pageSize: pageSize
+                pageSize: pageSize,
+                price: sortPrice,
+                num: sortNum
             },
-            success: function(info) {
-                // console.log(info);
-                $(".product ul").html(template("productTmp", info));
+            success: function (info) {
+                console.log(info);
+                callback(info);
             }
         });
     }
